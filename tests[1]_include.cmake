d880ec01e16d011d31450d79aa1ca79ce52ebea0
Ignacio Pardo <65306107+IgnacioPardo@users.noreply.github.com>
2022-03-17 13:50:46 -0300

    Delete tests[1]_include.cmake

--- tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/runner/ChartreuseIvoryDisassembly/tests[1]_tests.cmake")
-  include("/home/runner/ChartreuseIvoryDisassembly/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
